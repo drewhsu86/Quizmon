c5290c7dfe3c3175b2899653f27d7407042c0320
drewhsu86 <bowserhsu@gmail.com>
2020-06-11 10:16:39 -0400

    added validations for questions

--- quizmon/app/models/question.rb
+++ quizmon/app/models/question.rb
@@ -3,4 +3,7 @@ class Question < ApplicationRecord
   belongs_to :topic
   has_many :comments 
   has_many :completeds
+  
+  validates :correct_answer, inclusion: { in: %w(a b c d), message: "%{value} is not a valid answer."}
+  validates :difficulty, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
 end
--- quizmon/app/models/user.rb
+++ quizmon/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, confirmation: true 
 
   has_many :questions, dependent: :destroy
 end
